6ba2048d3860c24f66f04618b9323deb0f97e795
Joel Flores <j_flores88@outlook.com>
2021-02-26 15:25:33 -0800

    Added suit images.

--- blackJack.js
+++ blackJack.js
@@ -4,13 +4,19 @@ let cardSuit;
 let hand = [];
 let handValue = 0;
 
+
 const dealButton = document.getElementById('dealHand')
 const hitButton = document.getElementById('hitButton')
 const playerHand = document.getElementById('playerHand')
 const playerHandTotal = document.getElementById('playerHandTotal')
+const suitImages = document.getElementById('suitImgs')
 const deck = {
     values: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'Ace'],
-    suits: ['Diamonds', 'Spades', 'Clubs', 'Hearts']
+    suits: ['Diamonds', 'Spades', 'Clubs', 'Hearts'],
+    heart: document.getElementById('heartImg'),
+    diamond: document.getElementById('diamondsImg'),
+    spade: document.getElementById('spadesImg'),
+    club: document.getElementById('clubsImg')
 }
 
 const getCardValue = () => {
@@ -29,23 +35,54 @@ const getCardValue = () => {
     }
     hand.push(cardNumber)
     handValue += cardValue;
+    return cardNumber
 }
 
 const getCardSuit = () => {
+    let img = document.createElement('img');
+    img.style.width = '20px'
     const randomNumber = Math.floor(Math.random() * deck.suits.length)
     for(let i = 0; i < deck.suits.length; i++) {
         if(randomNumber === i) {
             cardSuit = deck.suits[i]
+            switch(cardSuit){
+                case 'Hearts':
+                    img.setAttribute('src', "./imgs/hearts.png");
+                    img.setAttribute('class', 'red')
+                    //drawcard(img) ???
+                    break;
+                case 'Diamonds':
+                    img.setAttribute('src', './imgs/diamonds.png');
+                    img.setAttribute('class', 'red')
+                    break;
+                case 'Spades':
+                    img.setAttribute('src', './imgs/spades.png');
+                    img.setAttribute('class', 'black');
+                    break;
+                case 'Clubs':
+                    img.setAttribute('src', './imgs/clubs.png');
+                    img.setAttribute('class', 'black');
+                    break;
+            }
         }
     }
     hand.push(cardSuit)
+    return img
 }
 
 const drawCard = () => {
-    getCardValue()
-    getCardSuit()
-    playerHand.innerText += ` ${cardNumber} of ${cardSuit} `;
-    playerHandTotal.innerText = `Hand Total: ${handValue}`;
+    updateDis(getCardValue(), getCardSuit())
+}
+
+const updateDis = (value, suit) => {
+    console.log(value, suit)
+    let cardSpan = document.createElement('span');
+    cardSpan.innerHTML = ` ${value}`, cardSpan.appendChild(suit)
+    playerHandTotal.innerText = `Total: ${handValue}`
+    if(suit.classList.contains('red')) {
+        cardSpan.style.color = 'red'
+    }
+    playerHand.append(cardSpan)
 }
 
 dealButton.onclick = () => {
