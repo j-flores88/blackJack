10ae1718c3a45a9cf3b477bd1463d4e38fc496be
Joel Flores <j_flores88@outlook.com>
2021-03-16 16:32:21 -0700

    Now keeps track of cards in deck.

--- blackJack.js
+++ blackJack.js
@@ -1,92 +1,103 @@
 let cardNumber;
 let cardValue;
 let cardSuit;
-let hand = [];
 let handValue = 0;
+
 
 
 const dealButton = document.getElementById('dealHand')
 const hitButton = document.getElementById('hitButton')
-const playerHand = document.getElementById('playerHand')
+const playerDisplay = document.getElementById('playerHand')
 const playerHandTotal = document.getElementById('playerHandTotal')
 const suitImages = document.getElementById('suitImgs')
-const deck = {
+const singleDeckBtn = document.getElementById('singleDeck')
+
+const gameDeck = {
+    deck: [],
+    hand: [],
+    discardPile: [],
     values: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'Ace'],
-    suits: ['Diamonds', 'Spades', 'Clubs', 'Hearts']
-}
-
-const getCardValue = () => {
-    const randomNumber = Math.floor(Math.random() * deck.values.length)
-    for(let i = 0; i < deck.values.length; i++) {
-        if(randomNumber === i) {
-            cardNumber = deck.values[i]
-        if(deck.values[i] === 'J' || deck.values[i] === 'Q' || deck.values[i] === 'K') {
-                cardValue = 10;
-            } else if (deck.values[i] === 'Ace') {
-                cardValue = 11
-            } else {
-                cardValue = deck.values[i]
+    suits: ['Diamonds', 'Spades', 'Clubs', 'Hearts'],
+    makeDeck(){
+        const {suits, values, deck} = this;
+        for(let value of values) {
+            for(let suit of suits) {
+                deck.push({value, suit});
             }
         }
-    }
-    hand.push(cardNumber)
-    handValue += cardValue;
-    return cardNumber
-}
-
-const getCardSuit = () => {
-    let img = document.createElement('img');
-    img.style.width = '20px'
-    const randomNumber = Math.floor(Math.random() * deck.suits.length)
-    for(let i = 0; i < deck.suits.length; i++) {
-        if(randomNumber === i) {
-            cardSuit = deck.suits[i]
-            switch(cardSuit){
-                case 'Hearts':
-                    img.setAttribute('src', "./imgs/hearts.png");
-                    img.setAttribute('class', 'red')
-                    //drawcard(img) ???
-                    break;
-                case 'Diamonds':
-                    img.setAttribute('src', './imgs/diamonds.png');
-                    img.setAttribute('class', 'red')
-                    break;
-                case 'Spades':
-                    img.setAttribute('src', './imgs/spades.png');
-                    img.setAttribute('class', 'black');
-                    break;
-                case 'Clubs':
-                    img.setAttribute('src', './imgs/clubs.png');
-                    img.setAttribute('class', 'black');
-                    break;
-            }
+        return deck;
+    },
+    drawCard() {
+        const {hand} = this; 
+        const drawnCard = this.deck.pop();  
+        this.discardPile.push(drawnCard);
+        hand.push(drawnCard)
+        this.updateDisplay(drawnCard)
+    },
+    dealHand(numCards){
+        for(let i = 0; i < numCards; i++) {
+            this.drawCard();
         }
+    },
+    shuffleDeck() {
+        const {deck} = this
+        for(let i = deck.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+        }
+    },
+    updateDisplay(card) {
+        let img = document.createElement('img');
+        img.style.width = '20px'
+    
+        cardNumber = card.value
+        cardSuit = card.suit
+    
+        if(cardNumber === 'J' || cardNumber === 'Q' || cardNumber === 'K') {
+            cardValue = 10;
+        } else if (cardNumber === 'Ace') {
+            cardValue = 11;
+        } else {
+            cardValue = cardNumber
+        }
+        handValue += cardValue
+    
+        switch(cardSuit) {
+            case 'Hearts':
+                img.setAttribute('src', "./imgs/hearts.png");
+                img.setAttribute('class', 'red')
+                break;
+            case 'Diamonds':
+                img.setAttribute('src', './imgs/diamonds.png');
+                img.setAttribute('class', 'red')
+                break;
+            case 'Spades':
+                img.setAttribute('src', './imgs/spades.png');
+                break;
+            case 'Clubs':
+                img.setAttribute('src', './imgs/clubs.png');
+                break;
+        }
+        let cardSpan = document.createElement('span');
+        cardSpan.innerHTML = `${cardNumber}`, cardSpan.appendChild(img)
+        playerHandTotal.innerText = `Total: ${handValue}`
+        if(img.classList.contains('red')) {
+            cardSpan.style.color = 'red'
+        }
+        playerDisplay.append(cardSpan)
     }
-    hand.push(cardSuit)
-    return img
 }
 
-const drawCard = () => {
-    updateDis(getCardValue(), getCardSuit())
+singleDeckBtn.onclick = () => {
+    gameDeck.makeDeck();
+    gameDeck.shuffleDeck();
 }
-
-const updateDis = (value, suit) => {
-    let cardSpan = document.createElement('span');
-    cardSpan.innerHTML = ` ${value}`, cardSpan.appendChild(suit)
-    playerHandTotal.innerText = `Total: ${handValue}`
-    if(suit.classList.contains('red')) {
-        cardSpan.style.color = 'red'
-    }
-    playerHand.append(cardSpan)
-}
-
 dealButton.onclick = () => {
-    playerHand.innerText = ''
+    playerDisplay.innerText = ''
     handValue = 0;
-    drawCard()
-    drawCard()
+    gameDeck.hand = [];
+    gameDeck.dealHand(2);
 }
-
 hitButton.onclick = () => {
-    drawCard()
+    gameDeck.drawCard();
 }
